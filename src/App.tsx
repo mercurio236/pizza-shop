10f6850c122501c0f95daebeb940b1d0d66428a1
Arley Souto <macx15@hotmail.com>
2024-01-02 22:41:27 -0300

    chore: adicionado Healm

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { RouterProvider } from "react-router-dom";
 import { Button } from "./components/ui/button";
 import "./global.css";
+import { router } from "./pages/routes";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 export function App() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <Button>Enviar</Button>
-    </div>
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | Pizza Shop"/>
+      <RouterProvider router={router} />
+    </HelmetProvider>
   );
 }
---
+++ src/pages/routes.tsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Dashboard } from "./app/dashboard";
+import { SignIn } from "./auth/sign-in";
+import { AppLayout } from "./_layouts/app";
+import { AuthLayout } from "./_layouts/auth";
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "sign-in",
+        element: <SignIn />,
+      },
+    ],
+  },
+]);
