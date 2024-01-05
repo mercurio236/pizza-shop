eb65eaf8e8369b7eea6aea0dd8b651f101adacbc
Arley Souto <macx15@hotmail.com>
2024-01-05 12:56:46 -0300

    feat: criado header e link

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,12 +1,13 @@
+import { Header } from "@/components/header";
 import { Outlet } from "react-router-dom";
 
-export function AppLayout(){
-    return(
-        <div>
-            <h1>Dashboard</h1>
-            <div>
-                <Outlet/>
-            </div>
-        </div>
-    )
+export function AppLayout() {
+  return (
+    <div className="flex min-h-screen flex-col antialiased">
+      <Header />
+      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
+        <Outlet />
+      </div>
+    </div>
+  );
 }
