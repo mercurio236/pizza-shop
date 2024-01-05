bacd24f722d5c0cb62d7dee31a12f5df538ab55c
Arley Souto <macx15@hotmail.com>
2024-01-05 19:35:10 -0300

    feat: order detail criado

---
+++ src/pages/app/orders/order-details.tsx
@@ -0,0 +1,12 @@
+import { DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+
+export default function OrderDetails() {
+  return (
+    <DialogContent>
+        <DialogHeader>
+            <DialogTitle>Pedido: ad908daosdia8doa9s8d</DialogTitle>
+            <DialogDescription>Detalhe do pedido</DialogDescription>
+        </DialogHeader>
+    </DialogContent>
+  )
+}
