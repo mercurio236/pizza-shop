a3a345d71850939e5f3b2c7c8a79918ad89cb7c0
Arley Souto <macx15@hotmail.com>
2024-01-05 20:10:31 -0300

    feat: criado cards

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Dashboard } from "./app/dashboard";
+import { Dashboard } from "./app/dashboard/dashboard";
 import { SignIn } from "./auth/sign-in";
 import { AppLayout } from "./_layouts/app";
 import { AuthLayout } from "./_layouts/auth";
