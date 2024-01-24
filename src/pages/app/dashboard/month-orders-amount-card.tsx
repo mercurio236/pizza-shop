c1e96d106f282667498563f56161a030868f6372
Arley Souto <macx15@hotmail.com>
2024-01-24 12:46:25 -0300

    fix: corrigido erro

--- src/pages/app/dashboard/month-orders-amount-card.tsx
+++ src/pages/app/dashboard/month-orders-amount-card.tsx
@@ -12,7 +12,7 @@ export default function MonthOrdersAmountCard() {
   return (
     <Card>
       <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-base">Predidos no (mês)</CardTitle>
+        <CardTitle className="text-base">Pedidos no (mês)</CardTitle>
         <Utensils className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
